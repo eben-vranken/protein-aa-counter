357f3af24e6fd0c6c5e6db0a5b268ece18398579
eben-vranken <vranken.eben@outlook.com>
2026-07-18 12:59:57 +0200

    feat: fasta parsing with mock fasta file

--- amino-acid-counter.py
+++ amino-acid-counter.py
@@ -1,16 +1,35 @@
 from argparse import ArgumentParser
 from src import counter
+from src import parser
 
 def parse_args():
     parser = ArgumentParser()
 
-    parser.add_argument("sequence", help="The protein sequence to be counted.")
+    parser.add_argument("-s", "--sequence", help="The protein sequence to be counted.")
+    parser.add_argument("-f", "--file", help="File path to FASTA protein file.")
+    
+    args = parser.parse_args()
+    
+    if not args.file and not args.sequence:
+        print("Either file or sequence flag is required.")
+        exit(1)
+    
+    if args.file and args.sequence:
+        print("Using both flag and sequence flags in one command is not allowed.")
+        exit(1)
 
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
-    sequence = args.sequence
+
+    if args.file:
+        sequence = parser.parse_fasta(args.file)
+        print(sequence)
+    else:
+        sequence = args.sequence
+
+
 
     # These two functions could definitely be combined into 1.
     # That would make this counter be O(2N) instead of O(N)
